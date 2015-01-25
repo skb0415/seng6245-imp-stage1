a9d9e7dece14cec01b159038a4b3dd14a0cf3eb3
Shawn Braddy <braddys03@students.ecu.edu>
2015-01-24 20:23:34 -0500

    Homework 1 - Getting Started - Shawn Braddy

--- test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
+++ test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
@@ -20,5 +20,29 @@ public class InterpreterTest {
 	public void testMultiply() {
 		IntegerValue iv = interpreter.interpret("1*2");
 		assertEquals("1*2", 2, iv.getIntValue().intValue());
-	}	
+	}
+
+	@Test
+	public void testSum() {
+		IntegerValue iv = interpreter.interpret("1+2");
+		assertEquals("1+2", 3, iv.getIntValue().intValue());
+	}
+	@Test
+	public void testMinus() {
+		IntegerValue iv = interpreter.interpret("3-2");
+		assertEquals("3-2", 1, iv.getIntValue().intValue());
+	
+}
+	@Test
+	public void testDivision() {
+		IntegerValue iv = interpreter.interpret("100/50");
+		assertEquals("100/50", 2, iv.getIntValue().intValue());
+	
+}
+	@Test
+	public void uranyMinus() {
+		IntegerValue iv = interpreter.interpret("10--2");
+		assertEquals("10--2", 12, iv.getIntValue().intValue());
+	
+}
 }
